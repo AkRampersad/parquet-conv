3099277dfdf5d582bc281d9e7d4b72196b6aa2c3
AkRampersad <93301335+AkRampersad@users.noreply.github.com>
2023-03-07 16:36:59 -0500

    Update parq_con.py
    updated code to fall more in line with example of module installation shown here https://colab.research.google.com/drive/1_KXTrLm9-wlJn8FQQqHu8CADp-uDHeSZ?usp=sharing#scrollTo=XzJ8Zw-D7t8y

--- parq_con.py
+++ parq_con.py
@@ -1,36 +1,17 @@
-from typing import Tuple
+import oakvar
 import duckdb
 from oakvar import BaseConverter
 
 
 
-class ParquetConverter(object):
+class Converter(BaseConverter):    
+    def check_format(self,file):
+        return file.name.endswith('.parquet') or file.name.endswith('.parquet.gz')
     
-    def __init__(self) -> None:
-        self.format_name = 'parquet'
-    
-    def check_format(self,f):
-        return f.name.endswith('.parquet') or f.name.endswith('.parquet.gz')
-        
-        
-    def setup(self,f,chunk) ->Tuple[BaseConverter]:
+    def convert_file(self,file,start):
+
         conn = duckdb.connect()
 
-
-        #formated duckDB query strings for easy insertion 
-        QUERY = '''SELECT * FROM {0} LIMIT {c} OFFSET {num}'''
-        count_q= ''' SELECT COUNT(*) FROM {0} '''  # counts row of parquet very fast
-
-        #duckDB queries with formated num from setup method
-        count = conn.execute(count_q.format(f)).df().to_dict()['count_star()'][0] 
-                 
-
-        counter = 0
-
-        while counter < count:
-            conn.execute(QUERY.format(f,c=chunk,num=counter))
-            counter+=chunk
-
-
-
-        
+        QUERY = f'''SELECT * FROM {file} LIMIT 1000 OFFSET {start-1}'''
+        count = conn.execute(QUERY).df().to_dict('index')
+        return count
